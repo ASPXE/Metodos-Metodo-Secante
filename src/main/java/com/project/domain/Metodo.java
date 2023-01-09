e966af3757e1eaa199e6405d5f9a5100c282baf2
ASPXE <aspxelite@outlook.com>
2023-01-09 07:25:29 -0600

    Menu casi terminado, clase que hace los calculos no terminada.

---
+++ src/main/java/com/project/domain/Metodo.java
@@ -0,0 +1,25 @@
+package com.project.domain;
+
+/**
+ *
+ * @author aspxe
+ */
+public class Metodo {
+    
+    public double elevar(double coeficiente, double exponente){
+        
+    }
+    
+    public double calcularGrados(double grado6, double grado5, double grado4, double grado3, double grado2, double grado1, double constante, double intervaloA, double intervaloB, double porcentajeError){
+        
+    }
+    
+    public double calcularRadianes(){
+        
+    }
+    
+    public double calcularGradosMinutoSegundos(){
+        
+    }
+    
+}
